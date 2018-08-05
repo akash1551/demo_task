9dc733fbd9f5d146e811b7f54bcc0df79e62c207
akash1551 <acwankhede@gmail.com>
2018-08-05 10:45:59 +0530

    added bulk create logic

---
+++ demo_task/helper_functions.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from demo_task.models import CultureAndRecreation
+from django.db import transaction
+
+
+def create_culture_and_recreation(filepath):
+    df = pd.read_csv(filepath)
+    df.columns = ['title', 'release_year', 'location', 'fun_fact',
+                  'production_company', 'distributor', 'director', 'writer',
+                  'actor1', 'actor2', 'actor3']
+
+    values = df.T.to_dict().values()
+    print('values: ', values)
+    data = [CultureAndRecreation(title=val['title'], release_year=val['release_year'],
+                                  location=val['location'], fun_fact=val['fun_fact'],
+                                  production_company=val['production_company'], distributor=val['distributor'],
+                                  director=val['director'], writer=val['writer'], actor1=val['actor1'],
+                                  actor2=val['actor2'], actor3=val['actor3']) for val in values]
+    with transaction.atomic():
+        CultureAndRecreation.objects.bulk_create(data)
+
+    return True
